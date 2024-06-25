23b0d3d52faf3670ffe7cb396a92022718571b59
bifinbabu <bifinbabu088@gmail.com>
2024-06-25 22:59:49 +0530

    One more question solved

--- 14_RenameKeysInObjext.js
+++ 14_RenameKeysInObjext.js
@@ -1,6 +1,15 @@
 // 14. Write a JavaScript program to replace multiple object keys' names with the values provided.
 
-const rename_keys = (change, obj) => {};
+const rename_keys = (change, obj) => {
+  return Object.keys(obj).reduce(
+    (acc, key) => ({
+      ...acc,
+      //   ...{ [change[key] ?? key]: obj[key] }, // Rename keys based on the keysMap, otherwise keep the original key - Both methods will give the desired output
+      ...{ [change[key] || key]: obj[key] }, // Rename keys based on the keysMap, otherwise keep the original key - Both methods will give the desired output
+    }),
+    {}
+  );
+};
 
 // Original object
 const obj = { name: "Bobo", job: "Programmer", shoeSize: 100 };
