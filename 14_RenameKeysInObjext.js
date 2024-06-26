a1cde0e981e9bd0d9e2ef267f12da0ed460d9679
bifinbabu <bifinbabu088@gmail.com>
2024-06-26 16:16:10 +0530

    Learnings added for question 14

--- 14_RenameKeysInObjext.js
+++ 14_RenameKeysInObjext.js
@@ -6,10 +6,29 @@ const rename_keys = (change, obj) => {
       ...acc,
       //   ...{ [change[key] ?? key]: obj[key] }, // Rename keys based on the keysMap, otherwise keep the original key - Both methods will give the desired output
       ...{ [change[key] || key]: obj[key] }, // Rename keys based on the keysMap, otherwise keep the original key - Both methods will give the desired output
+      //   In JavaScript, the || operator (logical OR) is used to return the first "truthy" value.
     }),
     {}
   );
 };
+
+// Iterating Over obj
+// Iteration 1:
+
+// key = "name"
+// change["name"] = "firstName" (truthy)
+// Result: ["firstName"]: obj["name"] (which is ["firstName"]: "Bobo")
+
+// Iteration 2:
+
+// key = "job"
+// change["job"] = "Actor" (truthy)
+// Result: ["Actor"]: obj["job"] (which is ["Actor"]: "Programmer")
+// Iteration 3:
+
+// key = "shoeSize"
+// change["shoeSize"] is undefined (falsy)
+// Result: ["shoeSize"]: obj["shoeSize"] (which is ["shoeSize"]: 100)
 
 // Original object
 const obj = { name: "Bobo", job: "Programmer", shoeSize: 100 };
