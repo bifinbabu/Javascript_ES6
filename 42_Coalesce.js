c0387807d03656dcfbecf7380e1d41a76610548e
bifinbabu <bifinbabu088@gmail.com>
2024-07-19 20:52:20 +0530

    Higher order function method added for question 42

--- 42_Coalesce.js
+++ 42_Coalesce.js
@@ -4,3 +4,19 @@ const customCoalesce = (...args) =>
   args.map((i) => (i ? i : false)).find((item) => item !== false);
 
 console.log(customCoalesce(undefined, null, NaN, "", "Waldo")); // Output: 'Waldo'
+
+// Higher order function method
+const coalesceFactory =
+  (valid) =>
+  // The returned function takes any number of arguments 'args'.
+  (...args) =>
+    // Find and return the first argument that passes the validity check.
+    args.find(valid);
+
+// Define a custom coalesce function 'customCoalesce' using 'coalesceFactory'.
+const customCoalesce1 = coalesceFactory(
+  (_) => ![null, undefined, "", NaN].includes(_)
+);
+
+// Test the 'customCoalesce1' function with various arguments and output the result.
+console.log(customCoalesce1(undefined, null, NaN, "", "Waldo")); // Output: 'Waldo'
